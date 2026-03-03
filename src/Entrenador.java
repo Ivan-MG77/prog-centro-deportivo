db949532b995496681b4a980ccf13fc38d95a698
ivanm <ivan.m.g.1791@gmail.com>
2026-03-03 09:20:28 +0100

    docs(Entrenador): Documentacion de clase Entrenador

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -1,10 +1,18 @@
 public class Entrenador {
 
+    //Atributos de la clase Entrenador
     private int id;
     private String nombre;
     private String especialidad;
     private int anosExperiencia;
 
+    /**
+     * Metodo COnstructor de la Clase, Siempre En PUBLIC
+     * @param id
+     * @param nombre
+     * @param especialidad
+     * @param anosExperiencia
+     */
     public Entrenador(int id, String nombre, String especialidad, int anosExperiencia) {
         this.id = id;
         this.nombre = nombre;
@@ -12,22 +20,43 @@ public class Entrenador {
         this.anosExperiencia = anosExperiencia;
     }
 
+    /**
+     * Metodo Get del ID
+     * @return this.id el id
+     */
     public int getId() {
         return this.id;
     }
 
+    /**
+     * Metodo Get del Nombre
+     * @return this.nombre
+     */
     public String getNombre() {
         return this.nombre;
     }
 
+    /**
+     * Metodo Get de Especialidad
+     * @return this.especialidad
+     */
     public String getEspecialidad() {
         return this.especialidad;
     }
 
+    /**
+     * Metodo Get de Años de Experiencia
+     * @return this.anosExperiencia
+     */
     public int anosExperiencia() {
         return this.anosExperiencia;
     }
 
+    /**
+     * Metodo toString de la Clase Entrenador para pasar todos sus datos en un String
+     * Siempre PUBLIC
+     * @return sb.toString(); Devuelve tod0 el metodo para que se pase todos los datos
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("Entrenador:");
