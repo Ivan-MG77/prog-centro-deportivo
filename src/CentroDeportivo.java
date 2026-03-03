f6cea89492ac6f4c176f3eb1d7962e2b96d91285
ivanm <ivan.m.g.1791@gmail.com>
2026-03-03 09:30:12 +0100

    feat(CentroDeportivo): Atrivutos y Metodo Constructor de la Clase CentroDeportivo

--- src/CentroDeportivo.java
+++ src/CentroDeportivo.java
@@ -1,2 +1,11 @@
 public class CentroDeportivo {
+
+    private Entrenador[] entrenadores;
+    private final int NUM_MAX_ENTRENADORES;
+
+
+    public CentroDeportivo(int NUM_MAX_ENTRENADORES) {
+        this.NUM_MAX_ENTRENADORES = NUM_MAX_ENTRENADORES;
+        entrenadores = new Entrenador[NUM_MAX_ENTRENADORES];
+    }
 }
